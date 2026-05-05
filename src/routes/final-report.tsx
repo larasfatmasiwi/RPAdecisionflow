1637c0748e418120605746a434d9dfc5a71fe826
larasfatmasiwi <lnf2126@columbia.edu>
2026-05-05 02:23:44 -0400

    Use Excel-linked final report page

--- src/routes/final-report.tsx
+++ src/routes/final-report.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { FinalReportPage } from '@/components/FiveStepPages'
+import { ExcelLinkedFinalReportPage } from '@/components/ExcelLinkedPages'
 
 export const Route = createFileRoute('/final-report')({
-  component: FinalReportPage,
+  component: ExcelLinkedFinalReportPage,
 })
